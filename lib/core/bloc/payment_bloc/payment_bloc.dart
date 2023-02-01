a7f0a12fd87757797eae49c1fbcbb0fca55dfae4
Burhan Kaynak <burhan.kaynak@synapsis.com.tr>
2023-02-01 07:38:53 +0300

    .

--- lib/core/bloc/payment_bloc/payment_bloc.dart
+++ lib/core/bloc/payment_bloc/payment_bloc.dart
@@ -1,11 +1,104 @@
+import 'dart:convert';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
 
 part 'payment_event.dart';
 part 'payment_state.dart';
 
 class PaymentBloc extends Bloc<PaymentEvent, PaymentState> {
-  PaymentBloc() : super(PaymentInitial()) {
-    on<PaymentEvent>((event, emit) {});
+  final Stripe _stripe = Stripe.instance;
+
+  PaymentBloc() : super(const PaymentState()) {
+    on<PaymentStart>(_onPaymentStart);
+    on<PaymentCreateIntent>(_onPaymentCreateIntent);
+    on<PaymentConfirmIntent>(_onPaymentConfirmMethod);
+  }
+
+  void _onPaymentStart(PaymentStart event, Emitter<PaymentState> emit) async {
+    emit(state.copyWith(status: PaymentStatus.initial));
+  }
+
+  void _onPaymentCreateIntent(
+      PaymentCreateIntent event, Emitter<PaymentState> emit) async {
+    emit(state.copyWith(status: PaymentStatus.loading));
+
+    final paymentMethod = await _stripe.createPaymentMethod(
+        params: PaymentMethodParams.card(
+            paymentMethodData:
+                PaymentMethodData(billingDetails: event.billingDetails)));
+
+    final paymentIntentResults = await _callPayEndpointMethodId(
+        useStripe: true,
+        paymentMethodId: paymentMethod.id,
+        currency: 'try',
+        items: event.items);
+
+    if (paymentIntentResults['error'] != null) {
+      emit(state.copyWith(status: PaymentStatus.failure));
+    }
+
+    if (paymentIntentResults['clientSecret'] != null &&
+        paymentIntentResults['requiresAction'] == null) {
+      emit(state.copyWith(status: PaymentStatus.succes));
+    }
+    if (paymentIntentResults['clientSecret'] != null &&
+        paymentIntentResults['requiresAction'] == true) {
+      final String clientSecret = paymentIntentResults['clientSecret'];
+      add(PaymentConfirmIntent(clientSecret: clientSecret));
+    }
+  }
+
+  void _onPaymentConfirmMethod(
+      PaymentConfirmIntent event, Emitter<PaymentState> emit) async {
+    try {
+      final paymentIntent = await _stripe.handleNextAction(event.clientSecret);
+      if (paymentIntent.status == PaymentIntentsStatus.RequiresConfirmation) {
+        var results =
+            await _callPayEndpointIntent(paymentIntentId: paymentIntent.id);
+
+        if (results['error'] != null) {
+          emit(state.copyWith(status: PaymentStatus.failure));
+        } else {
+          emit(state.copyWith(status: PaymentStatus.succes));
+        }
+      }
+    } catch (e) {
+      print(e);
+      emit(state.copyWith(status: PaymentStatus.failure));
+    }
+  }
+
+  Future<Map<String, dynamic>> _callPayEndpointIntent(
+      {required paymentIntentId}) async {
+    final url = Uri.parse(
+        'https://us-central1-help-me-to-speak-dev.cloudfunctions.net/StripePayEndPointIntentId');
+
+    final response = await http.post(url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({'paymentIntentId': paymentIntentId}));
+
+    return json.decode(response.body);
+  }
+
+  Future<Map<String, dynamic>> _callPayEndpointMethodId(
+      {required useStripe,
+      required paymentMethodId,
+      required currency,
+      required items}) async {
+    final url = Uri.parse(
+        'https://us-central1-help-me-to-speak-dev.cloudfunctions.net/StripePayEndPointMethodId');
+
+    final response = await http.post(url,
+        headers: {'Content-Type': 'application/json'},
+        body: json.encode({
+          'useStripe': useStripe,
+          'paymentMethodId': paymentMethodId.id,
+          'currency': currency,
+          'items': items
+        }));
+    return json.decode(response.body);
   }
 }
--- lib/core/bloc/payment_bloc/payment_event.dart
+++ lib/core/bloc/payment_bloc/payment_event.dart
@@ -6,3 +6,21 @@ abstract class PaymentEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class PaymentStart extends PaymentEvent {}
+
+class PaymentCreateIntent extends PaymentEvent {
+  final BillingDetails billingDetails;
+  final List<Map<String, dynamic>> items;
+  const PaymentCreateIntent(
+      {required this.billingDetails, required this.items});
+  @override
+  List<Object> get props => [billingDetails, items];
+}
+
+class PaymentConfirmIntent extends PaymentEvent {
+  final String clientSecret;
+  const PaymentConfirmIntent({required this.clientSecret});
+  @override
+  List<Object> get props => [clientSecret];
+}
